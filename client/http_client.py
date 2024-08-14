74c7e98bd3b2d8d4c346ecc8dd6720bc9a53ed03
pavel <pmhomenko@yandex.ru>
2024-08-14 09:55:47 +0500

    fix: monthly candle request url

--- client/http_client.py
+++ client/http_client.py
@@ -1,10 +1,11 @@
 import requests
 
 
-def get_candles(ticker, api_key, candle_size, month):
+def get_candles(ticker, api_key, candle_size, date_candles):
     if candle_size == 24:
         url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&outputsize=full&apikey={api_key}'
     else:
+        month = date_candles.strftime("%Y-%m")
         url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={ticker}&interval={candle_size}min&month={month}&outputsize=full&apikey={api_key}'
     r = requests.get(url)
     data = r.json()
